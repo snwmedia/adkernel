eb30d96653e6dea2a82ab29dc8c35705c5e2ce38
Oded <odedhb@gmail.com>
2019-11-18 15:15:37 +0200

    init

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class AdKernel {
+    static authenticate() {
+        console.log('authenticate');
+    }
+}
+exports.AdKernel = AdKernel;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export class AdKernel {
+    static authenticate() {
+        console.log('authenticate')
+    }
+}
